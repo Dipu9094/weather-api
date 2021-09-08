e8b82147d9b7ce68beb4f75e3b6af6b0ba883e39
Dipu9094 <dipusultan090@gmail.com>
2021-09-09 02:02:26 +0600

    Weather icon added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const getWeather = async () => {
     const cityName = getInput.value;
     getInput.value = ''
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=31756088f95950e8db6e6e881f838615`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=31756088f95950e8db6e6e881f838615&units=metric`
     // fetch(url)
     //     .then(res => res.json())
     //     .then(data => showWeather(data))
@@ -23,9 +23,9 @@ const getWeather = async () => {
 const showWeather = data => {
     console.log(data);
     const weatherContainer = document.getElementById('weatherResult')
-    const temp = ((data.main.temp) - 273.15).toFixed(1);
+    const temp = ((data.main.temp))
     weatherContainer.innerHTML = `
-        <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="">
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
         <h1>${data.name}</h1>
         <h3><span>${temp}</span>&deg;C</h3>
         <h1 class="lead">${data.weather[0].description}</h1>
